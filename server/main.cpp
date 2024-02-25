bded0718f95eb01b38e71ce97cb9c47c5005c875
nejHate <63677856+nejHate@users.noreply.github.com>
2024-02-25 11:08:49 +0000

    add

--- server/main.cpp
+++ server/main.cpp
@@ -5,16 +5,53 @@
 #define volume_of_chunk width_of_chunk*height_of_chunk
 
 struct chunk{
+    chunk *up;
+    int up_ptr;
+    chunk *right;
+    int right_ptr;
+    chunk *down;
+    int down_ptr;
+    chunk *left;
+    int left_ptr;
     int chunk_blocks[volume_of_chunk];
     int changes[100];
 
+    chunk(){}
 
-    //chunk() :{}
+    chunk(chunk* up, chunk* right, chunk* down, chunk* left) :
+        up(up),
+        right(right),
+        down(down),
+        left(left),
+        up_ptr(0),
+        right_ptr(0),
+        down_ptr(0),
+        left_ptr(0)
+    {
+        if(up != nullptr){
+            up->down = this;
+        }
+        if(right != nullptr){
+            right->left = this;
+        }
+        if(down != nullptr){
+            down->up = this;
+        }
+        if(left != nullptr){
+            left->right = this;
+        }
+    }
 };
 
 int main(){
     chunk *loadded_chunk[10];
-    loadded_chunk[0] = new(chunk);
+    for (int i = 0; i < 10; i++){
+        loadded_chunk[i] = new chunk(nullptr, nullptr, nullptr, nullptr);
+    }
+    for (int i = 0; i < 10; i++){
+        delete loadded_chunk[i];
+    }
     //std::cout << loadded_chunk[0].x << std::endl;
     //std::cout << loadded_chunk[0].y << std::endl;
+    std::cout << "END OF PROGRAM" << std::endl;
 }
