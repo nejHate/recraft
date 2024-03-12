3f7f9441578f1ba6b11dd7660b83b9668071f704
nejhate <jakub.steb@gmail.com>
2024-03-12 15:18:17 +0100

    wtf add xdd

--- server/main.cpp
+++ server/main.cpp
@@ -5,15 +5,20 @@
 #include <fstream>
 #include <unistd.h>
 #include <filesystem>
+#include <cstring>
+#include <bitset>
+#include <thread>
+#include <atomic>
 
 
 #define width_of_chunk 16
 #define height_of_chunk 256
-#define volume_of_chunk width_of_chunk*width_of_chunk*height_of_chunk
-#define max_saved_chunk_size volume_of_chunk*sizeof(block)
+#define size_of_chunk width_of_chunk*width_of_chunk*height_of_chunk
+#define max_saved_chunk_size size_of_chunk*sizeof(block)
 
 struct block{
-    u_int32_t block;
+    std::bitset<32> block;
+    //void block;
     //int block2;
 };
 struct chunk{
@@ -27,7 +32,7 @@ struct chunk{
     int down_ptr;
     chunk *chunk_x_minus;
     int left_ptr;
-    block chunk_blocks[volume_of_chunk];
+    block chunk_blocks[size_of_chunk];
     int changes[100];
 
     chunk(){}
@@ -58,6 +63,21 @@ struct chunk{
         }
         std::cout << "creation of chunk: " << chunk_x << " " << chunk_z << std::endl;
     }
+    /*~chunk(){
+        if (chunk_z_minus != nullptr){
+            chunk_x_minus->chunk_z_plus = nullptr;
+        }
+        if(chunk_x_plus != nullptr){
+            chunk_x_plus->chunk_x_minus = nullptr;
+        }
+        if(chunk_z_plus != nullptr){
+            chunk_z_plus->chunk_z_minus = nullptr;
+        }
+        if(chunk_x_minus != nullptr){
+            chunk_x_minus->chunk_x_plus = nullptr;
+        }
+
+    }*/
 };
 class Chunk_Manager{
     private:
@@ -85,6 +105,17 @@ class Chunk_Manager{
         }
 };
 
+void print_chunk_blocks(chunk *chunk){
+    uint8_t *chachar = reinterpret_cast<uint8_t*>(chunk->chunk_blocks);
+    for (int i = 0; i < size_of_chunk; i++){
+        if(i != 0 && i % width_of_chunk*width_of_chunk == 0){
+            std::cout << std::endl;
+        }
+        std::cout << static_cast<unsigned int>(chachar[i]) << " ";
+    }
+    std::cout << std::endl;
+}
+
 
 bool file_exists(const std::string& filename){
     struct stat buffer;
@@ -100,7 +131,7 @@ chunk* create_chunk(int x, int z, Chunk_Manager &hashed_chunks){
     return(new_chunk);
 }
 
-chunk* load_chunk(std::string& base, int x, int z, Chunk_Manager &hashed_chunks, bool end){
+chunk* load_chunk(std::string &base, int x, int z, Chunk_Manager &hashed_chunks, bool end){
     static char *loaded_file = new(std::nothrow) char[max_saved_chunk_size];
     if (loaded_file == nullptr){
         std::cout << "ERROR TO ALLOCATE ARRAY FOR LOADING FILE" << " in file: " << __FILE__ << ":" << __LINE__ << std::endl;
@@ -120,16 +151,22 @@ chunk* load_chunk(std::string& base, int x, int z, Chunk_Manager &hashed_chunks,
         }
         std::error_code ec{};
         auto file_size = std::filesystem::file_size(filename, ec);
+        std::cout << "size of file is: " << file_size << std::endl;
         if (ec) {
             std::cerr << "ERROR WHEN GETING LENGHT OF FILE: " << filename << " in file: " << __FILE__ << ":" << __LINE__ << " " << ec.message() << std::endl;
         }
         file.read(loaded_file, file_size);
-        loaded_file[file_size] = '\0';  // DELETE don't needed when not printing
         std::cout << "size of file: " << file_size << std::endl;
 
-        std::cout << "file content: " << loaded_file << std::endl;
+        //std::cout << "file content: " << loaded_file << "<-END" << std::endl;
 
         chunk *new_chunk = create_chunk(x, z, hashed_chunks);
+        std::memcpy(new_chunk->chunk_blocks, reinterpret_cast<chunk*>(loaded_file), file_size);
+        std::cout << "content: ";
+        for(u_int64_t i = 0; i < file_size; i++){
+            break;
+            std::cout << (&new_chunk->chunk_blocks[i]) << std::endl;
+        }
         file.close();
         return new_chunk;
 
@@ -141,6 +178,30 @@ chunk* load_chunk(std::string& base, int x, int z, Chunk_Manager &hashed_chunks,
     }
 }
 
+void unload_chunk(std::string &base, chunk* chunk, Chunk_Manager &hashed_chunks, std::atomic<uint16_t*> *active_saving_chunk_threads){
+    std::cout << "start saving chunk: " << chunk->chunk_x << " " << chunk->chunk_z << std::endl;
+    std::string filename = base + std::to_string(chunk->chunk_x) + "." + std::to_string(chunk->chunk_z) + ".chunk";
+    std::ofstream file(filename, std::ios::binary);
+    if (!file.is_open()){
+        std::cout << "ERROR WHEN OPENING FILE" << std::endl;
+        exit(-1);
+    }
+    char *chachar = reinterpret_cast<char*>(&chunk->chunk_blocks);
+    file.write(chachar, size_of_chunk);
+    for(int i = 0; i < size_of_chunk; i++){
+        break;
+        file << chachar[i];
+    }
+    hashed_chunks.delete_chunk(chunk->chunk_x, chunk->chunk_z);
+    std::cout << "deleting chunk: " << chunk->chunk_x << " " << chunk->chunk_z << std::endl;
+    delete chunk;
+    file.close();
+    active_saving_chunk_threads->fetch_sub(1);
+}
+
+void fuck(std::atomic<uint16_t> *active_saving_chunk_thread){
+    active_saving_chunk_thread->fetch_sub(1);
+}
 
 
 int main(){
@@ -154,18 +215,25 @@ int main(){
 
     std::string base = "./data/";
     chunk *test;
+    std::atomic<uint16_t> active_saving_chunk_threads = 0;
 
-    int test_size = 100;
+    int test_size = 3;
     for(int i = 0; i < test_size; i++){
         for(int y = 0; y < test_size; y++){
             load_chunk(base, i, y, hashed_chunks, 0);
         }
     }
 
-    test = hashed_chunks.find_chunk(0, 0);
-    std::cout << test->chunk_z_minus << " " << test->chunk_x_plus << " " << test->chunk_z_plus << " " << test->chunk_x_minus << std::endl << std::endl;
+    test = hashed_chunks.find_chunk(0, 0);;
 
-    for(int i = 0; i < test_size; i++){
+    char *chachar = reinterpret_cast<char*>(hashed_chunks.find_chunk(0, 0)->chunk_blocks);
+    for(int i = 0; i < size_of_chunk; i++){
+        chachar[i] = i + 1;
+    }
+    //print_chunk_blocks(hashed_chunks.find_chunk(0, 0));
+
+    //testing
+    /*for(int i = 0; i < test_size; i++){
         test = hashed_chunks.find_chunk(0, i);
         for (int y = 0; y < test_size-1; y++){
             std::cout << test->chunk_x << test->chunk_z << " ";
@@ -189,19 +257,28 @@ int main(){
             test = test->chunk_z_minus;
         }
         std::cout << test->chunk_x << test->chunk_z << std::endl;
-    }
+    }*/
+
     for(int i = 0; i < test_size; i++){
         for(int y = 0; y < test_size; y++){
+            active_saving_chunk_threads.fetch_add(1);
+            //std::thread saving_thread(unload_chunk, std::ref(base), hashed_chunks.find_chunk(i, y), std::ref(hashed_chunks), &active_saving_chunk_threads);
+            fuck(&active_saving_chunk_threads);
+            //std::thread saving_thread(fuck, (&active_saving_chunk_threads));
+            //saving_thread.detach();
+            //save_thread.detach();
+            continue;
             delete (hashed_chunks.find_chunk(i, y));
             hashed_chunks.delete_chunk(i, y);
-            std::cout << "deleting chunk: " << i << " " << y << std::endl;
         }
     }
+    while(active_saving_chunk_threads){}
     if(load_chunk(base, 1, 1, hashed_chunks, 1)){
         std::cout << "ERROR WHEN CLEANING FUNCTION load_chunk " << " in file: " << __FILE__ << ":" << __LINE__ << std::endl;
     }
     std::cout << sizeof(chunk) << " " << sizeof(chunk) * test_size / 1000000 << std::endl;
     std::cout << "END OF PROGRAM #5" << std::endl << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
 }
 
 //clear && g++ -fwhole-program -O3 -g -fsanitize=address -fsanitize=leak main.cpp -o main && ./main && g++ ./main.cpp -g -o main && valgrind -s --leak-check=full ./main
